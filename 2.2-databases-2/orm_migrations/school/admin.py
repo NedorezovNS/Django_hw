e87be4ae7b4b11c203294da5a7519f31766e28a5
Nikita Nedorezov <n.s.nedorezov@gmail.com>
2024-01-30 00:03:40 +0300

    update admin.py

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+class TeachersInline(admin.TabularInline):
+    model = Student.teachers.through
+    extra = 1
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    inlines = [TeachersInline]
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    inlines = [TeachersInline]
